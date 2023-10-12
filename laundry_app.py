3549953e837d61fb076625ef6c2a570a538cb05e
VumGualnam <vumsuanmang@gmail.com>
2023-10-12 10:59:17 +0800

    Completed notification, temperature and basket capacity indicators

--- laundry_app.py
+++ laundry_app.py
@@ -1,42 +1,72 @@
-from flask import Flask, request, jsonify, render_template
-import sqlite3
+import json
+from functools import reduce
+import math
+from operator import add
+import random
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import smtplib, ssl
+import sqlite3, re
 
 app = Flask(__name__)
 
-# Default sensor distance
-default_sensor_distance = 45.00  # in cm
+# Default basket height
+actual_basket_height = 80.00 # unit: cm
+default_basket_height = actual_basket_height * 0.95 
 
-@app.route('/sensor_test', methods=['POST'])
-def posttome():
-    print(request.data)
-    return 'vum'
+default_recommend_temp = 0 # default: 0 for cold
 
-@app.route('/data', methods=['POST'])
-def receive_data():
-    data = request.json
-    print("Received data:", data)
 
-    # Update the SQLite database
-    with sqlite3.connect('clothes_db.sqlite') as conn:
-        cursor = conn.cursor()
-        cursor.execute("""
-                INSERT INTO Clothes (unique_id, clothes_type_id, washing_temperature_id, sensor_distance)
-                VALUES (?, ?, ?, ?)
-                """, (data['unique_id'], data['clothes_type_id'], data['washing_temperature_id'], data['sensor_distance']))
+@app.route('/user-email.html')
+def displayUserEmailForm():
+    return render_template("user-email.html")
+
+@app.route('/wifimanager.html')
+def displayWifimanager():
+    return render_template("wifimanager.html")
+
+@app.route('/sendEmail', methods=['POST'])
+def sendEmail():
+    # sample: {"message" : "This is a sample email message"}
+    message = json.loads(request.data)["message"] 
+    # print(message)
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = "sduri.pro@gmail.com"
+    receiver_email = "sduri.pro@gmail.com"
+    password = "ymwqjfefcsnfgzri" # input("Type your password and press enter:")
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
         
-        rows_affected = cursor.rowcount
-        print(f"Rows affected by update: {rows_affected}")
-        conn.commit()
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+    print("message : Email sent successfully!")
+    return jsonify({"message": "Email sent successfully!"})
 
-    if rows_affected == 0:
-        return jsonify({"error": f"No rows with unique_id {data['unique_id']} found!"}), 400
+# @app.route('/data', methods=['POST'])
+# def receive_data():
+#     data = request.json
+#     print("Received data:", data)
 
-    return jsonify({"message": "Data updated successfully!"})
+#     # Update the SQLite database
+#     with sqlite3.connect('clothes_db.sqlite') as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("""
+#                 INSERT INTO Clothes (unique_id, clothes_type_id, washing_temperature_id, sensor_distance)
+#                 VALUES (?, ?, ?, ?)
+#                 """, (data['unique_id'], data['clothes_type_id'], data['washing_temperature_id'], data['sensor_distance']))
+        
+#         rows_affected = cursor.rowcount
+#         print(f"Rows affected by update: {rows_affected}")
+#         conn.commit()
 
-@app.route('/test2', methods=['POST'])
-def test2():
-    print("post test")
-    # return "index.html"
+#     if rows_affected == 0:
+#         return jsonify({"error": f"No rows with unique_id {data['unique_id']} found!"}), 400
+
+#     return jsonify({"message": "Data updated successfully!"})
 
 @app.route('/sensor', methods=['POST'])
 def sensor_endpoint():
@@ -46,15 +76,18 @@ def sensor_endpoint():
     # Print the received value
     print(f"Received sensor value: {sensor_value_text}")
 
+    if int(sensor_value_text) > default_basket_height or int(sensor_value_text) <= 0:
+        return jsonify({"message": "Error sensor reading! Database is NOT updated!"})
+    
     # If you also want to use it as a float for further processing, you can convert it:
     try:
         sensor_value = float(sensor_value_text)
     except ValueError:
         return "not float"
     
-    with sqlite3.connect('clothes_db.sqlite') as conn:
+    with sqlite3.connect('washing_db_corrected.sqlite') as conn:
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO "Sensor Data" (sensor_reading) VALUES (?)', (sensor_value,))
+        cursor.execute('INSERT INTO "DistanceState" (distance) VALUES (?)', (sensor_value,))
         conn.commit()
 
         rows_affected = cursor.rowcount
@@ -63,14 +96,34 @@ def sensor_endpoint():
     if rows_affected == 0:  
         return jsonify({"error": f"error nothing happened"}), 400
     
-    return jsonify({"message": "Sensor Reading Data updated successfully!"})
-    
+    print("message: Sensor Reading Data updated successfully!")
+    return redirect(url_for('home', distance=math.ceil(sensor_value))) 
+    # return jsonify({"message": "Sensor Reading Data updated successfully!"})
 
 
 @app.route('/rfid', methods=['POST'])
 def rfid_endpoint():
     data = request.json
-    print(data)
+    # print(data)
+    tag_id = data['tag_id']
+    cloth_type = data['tag_info'].split(",")[0]
+    cloth_color = data['tag_info'].split(",")[1]
+    # print(cloth_type, cloth_color)
+    distance = 0 # can discuss later
+    
+    with sqlite3.connect('washing_db_corrected.sqlite') as conn:
+        cursor = conn.cursor()
+        
+        cursor.execute('INSERT INTO "TempInput" \
+                       (tag, cloth_type, distance, cloth_color) VALUES (?,?,?,?)', 
+                       (tag_id, cloth_type, distance, cloth_color))
+        conn.commit()
+
+        rows_affected = cursor.rowcount
+        print(f"Rows affected by update: {rows_affected}")
+        
+    if rows_affected == 0:  
+        return jsonify({"error": f"error nothing happened"}), 400
     
     # For now, just return a stubbed response
     return jsonify({"message": "RFID data processed!"})
@@ -79,40 +132,77 @@ def differentiate_clothes(tag_info):
     # Placeholder function for processing RFID tag information
     pass
 
-@app.route('/vum', methods=['GET'])
-def get_data():
-    data = request.json
-    print("Getted data:", data)
+def insert_into_db(table_name):
+    col_name, values = table_name
 
-@app.route('/test') 
-def test(): 
-    # return 'Hello, World! this application runing on 192.168.0.105'
-    return "hello"
+def get_table_from_db(table_names, colnames=None):
+    """
+    Returns all rows from a table as a list of lists
+    """
+    
+    desired_tables = []
+    with sqlite3.connect('washing_db_corrected.sqlite') as conn:
+        cursor = conn.cursor()
+
+        if type(table_names) is not list:
+            table_names = re.split(',|_|-|!', table_names.replace(" ",""))
+
+        if type(colnames) is not None and type(colnames) is  list:
+            colnames = re.split(',|_|-|!', colnames.replace(" ",""))
+               
+        for table_name in table_names:
+            # print(table_name)
+            
+            if colnames is None:
+                query = 'SELECT * FROM ' + table_name
+                desired_tables.append(conn.execute(query).fetchall())
+            else:
+                query = 'SELECT ' + ''.join(colnames) + ' FROM ' + table_name
+                # print("query: " + query)
+                desired_tables.append(conn.execute(query).fetchall())
+
+    # print("\ndesired_tables: \n", desired_tables)
+    if len(table_names) == 1:
+        return reduce(add, desired_tables)
+    else:
+        return desired_tables
+
+@app.route('/get_distance_val', methods=['POST', 'GET'])
+def get_distance_val():
+    # print("distance: " + str(get_table_from_db("DistanceState")[-1][1]))
+    return str(get_table_from_db("DistanceState")[-1][1])
+    # return str(random.randint(10,80)) # can be used for testing
+
+@app.route('/get_temp_val', methods=['POST', 'GET'])
+def get_temperature_val():
+    recommended_temp = get_table_from_db("Clothes", "washing_temperature_id")
+    #removing all non-type 
+    recommended_temp = [sub for sub in recommended_temp if not all(ele == None for ele in sub)]
+    recommended_temp = str(max(recommended_temp)[0])
+    recommended_temp = recommended_temp if  recommended_temp is not None else default_recommend_temp
+    print("recommended_temp: ", recommended_temp)
+    return recommended_temp
 
 @app.route('/content.html')
 def display_clothes():
     
     with sqlite3.connect('washing_db_corrected.sqlite') as conn:
         cursor = conn.cursor()
-        # cursor.execute("""
-                # SELECT * FROM TempInput
-                # """)
-        clothes = conn.execute('SELECT * FROM Clothes').fetchall()
-        colors = dict(conn.execute('SELECT * FROM ClothesColor').fetchall())
-        clothe_types = conn.execute('SELECT id, name FROM ClothType').fetchall()
+        clothes = get_table_from_db("Clothes") # conn.execute('SELECT * FROM Clothes').fetchall()
+        colors = dict(get_table_from_db("ClothesColor")) # dict(conn.execute('SELECT * FROM ClothesColor').fetchall())
+        clothe_types = get_table_from_db("ClothType") #conn.execute('SELECT id, name FROM ClothType').fetchall()
         clothe_types = dict([(i[0], i[1]) for i in clothe_types])
-        temp = dict(conn.execute('SELECT * FROM Temperature').fetchall())
-        print(type(clothe_types))
-        print(clothe_types)
+        temp = dict(get_table_from_db("Temperature") ) # dict(conn.execute('SELECT * FROM Temperature').fetchall())
 
         list_to_display = []
-        
+
         for cloth in clothes:
             temp_list = []
-            print(type(int(cloth[1]))) # str
-            temp_list.append(clothe_types[int(cloth[1])])
+            # print(int(cloth[1]))
+            # print(type(int(cloth[1]))) # str
+            temp_list.append(clothe_types[int(cloth[1])]) # cloth_type_id
             if cloth[-1] is not None:
-                temp_list.append(colors[cloth[-1]])
+                temp_list.append(colors[cloth[-1]]) # cloth_color_id
                 # print(colors[cloth[-1]])
             else:
                 # print("Unknown")
@@ -120,16 +210,31 @@ def display_clothes():
             # temp_list.append(cloth[2])
             # print(colors[i[-1]])
             # print(colors[list(i)[-1]])
-        # # list_to_display = 
+
             list_to_display.append(temp_list)
-        print(list_to_display)
+        # print(list_to_display)
     return render_template('/content.html', data=list_to_display)
 
-@app.route('/') 
-def home(): 
-    # return 'Hello, World! this application runing on 192.168.0.105'
-    return render_template('index.html', value=10)
 
+
+@app.route('/',  methods=['GET', 'POST'])
+@app.route('/<distance>', methods=['GET', 'POST'])
+@app.route('/index.html') 
+def home(distance=None): 
+
+    
+
+    distance_from_args = distance # request.args.get("distance")
+    # print("distance_from_args: ", distance_from_args)
+    distance = distance_from_args if distance_from_args is not None else get_table_from_db("DistanceState")[-1][1]
+    # print("distance_from_args::distance: ", distance)
+
+    if int(distance) <= 0.15 * default_basket_height:
+        distance = 0.15 * default_basket_height
+    return render_template('index.html', 
+                           distance_val=distance, 
+                           defaultBasketLength=default_basket_height
+                           )
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=5003)
--- static/js/script.js
+++ static/js/script.js
@@ -1,44 +1,135 @@
+///////////////////// start: functions /////////////////////////
 function moveTriangleByData(dataValue) {
   const triangle = document.getElementById("triangle");
-  let positions = [0, 50, 100]; // pixel as unit
+  // const temp_card = document.getElementsByClassName("temp_card");
+  let positions = [15, 60, 106, 200]; // pixel as unit
+  let text_indicators = ["Cold", "Cold/Warm", "Warm", "Do not wash"]
 
-  let positionIndex; //change if needed
-  if (dataValue <= 33) {
-    positionIndex = 0;
-  } else if (dataValue <= 66) {
-    positionIndex = 1;
-  } else {
-    positionIndex = 2;
+  // console.log("triangle:datavalue: ", dataValue);
+  let positionIndex = dataValue; //change this value to 0, 1, 2, or 3 to see the movement of the triangle
+  // if (dataValue <= 33) {
+  //   positionIndex = 0;
+  // } else if (dataValue <= 66) {
+  //   positionIndex = 1;
+  // } else {
+  //   positionIndex = 2;
+  // }
+
+  // console.log("positionIndex: " + positionIndex + " | dataValue: " + dataValue);
+  // (document.getElementsByClassName("temp-card")[0].textContent.split("\n")[4]).trim()
+  triangle.style.bottom = positions[positionIndex] + "px";
+  $("#temperature-text").text(text_indicators[positionIndex]);
+
+  if (positionIndex == 3) {
+    $("#notification").text(text_indicators[positionIndex]);
+    $("#notification").css("color", "red");
+    $("#fas-notification").addClass("fas fa-exclamation-triangle");
+    $("#fas-notification").css("color", "red");
   }
 
-  // triangle.style.bottom = positions[positionIndex] + "px";
-  triangle.style.bottom = positions[1] + "px";
+  if (positionIndex == 0) {
+    $("#notification").text(text_indicators[positionIndex]);
+    $("#notification").css("color", "green");
+    $("#fas-notification").addClass("fas fa-exclamation-triangle");
+    $("#fas-notification").css("color", "green");
+  }
+
+  // temp_card.style.css = positions[positionIndex] + "px";
 }
 
 function BasketFillByData(dataValue) {
-  let dataValue = 5;
-  const basketFill = document.getElementById("basketFill");
-  let heightP;
-  if (dataValue <= 0) {
-    heightP = "0%";
-  } else if (dataValue <= 4) {
-    heightP = "4%";
-  } else if (dataValue <= 8) {
-    heightP = "8%";
-  } else if (dataValue <= 12) {
-    heightP = "12%";
-  } else if (dataValue <= 16) {
-    heightP = "16%";
-  } else {
-    heightP = "20%";
-  }
-  basketFill.style.height = "20%"; // heightP;
+  // let dataValue = 5;
+  dataValue = Math.ceil(dataValue)
+  console.log("dataValue: " + dataValue)
+  var defaultBasketLength = document.getElementById("defaultBasketLength").value;
+  console.log("defaultBasketLength: " + defaultBasketLength)
+  // console.log("typeof(dataValue): " + typeof(dataValue))
+  // const basketFill = document.getElementById("basketFill");
+  
+  var basketImg = document.getElementById("basket");
+  var basketImgHeight = basketImg.height;
+  console.log("basketImgHeight: " + basketImgHeight);
+  
+  // let heightP = (dataValue/80) * 100  + "%";
+  var heightPercentage = Math.ceil((dataValue/defaultBasketLength) * 100);
+  // heightPercentage = parseInt(heightPercentage,10)
+  // let heightP = (dataValue/defaultBasketLength) * basketImgHeight;
+  // if (dataValue <= 0) {
+  //   heightP = "0%";
+  // } else if (dataValue <= 4) {
+  //   heightP = "4%";
+  // } else if (dataValue <= 8) {
+  //   heightP = "8%";
+  // } else if (dataValue <= 12) {
+  //   heightP = "12%";
+  // } else if (dataValue <= 16) {
+  //   heightP = "16%";
+  // } else {
+  //   heightP = "20%";
+  // }
+  // console.log("heightP: " + heightP );
+
+  // basketFill.style.height = heightP + "px";
+  var lgstyle = 'linear-gradient( to top, \
+      red ' + heightPercentage  + '%, \
+      white ' + heightPercentage  + '%, \
+      white ' + (100 - heightPercentage) + '%';
+  console.log("linear-gradient-style: " + lgstyle);
+  $("#basket").css({background: lgstyle});
 }
-
-
 
 function Notification(message) {
   const messageBox = document.getAnimations("messageBox");
 
   messageBox.innerHTML = message;
 }
+
+function fetchDistanceValue() {
+  
+  // $.ajax({ 
+  //   type : 'GET',
+  //   url : "/get_distance_val",
+  //   success: function(response){ 
+  //     // 'response' is the distance value
+  //     BasketFillByData(response);
+  //   } 
+  // }); 
+}
+
+
+function sendEmail(message){
+  $.ajax({
+    type: "POST",
+    url: "/sendEmail",
+    data: JSON.stringify({"message": message}),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    success: function(sendEmail_response) {
+      console.log("sendEmail_response: ", sendEmail_response);
+    }, 
+    failure: function(errMsg) {
+      alert(errMsg);
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+function getRecommendedTemp(){
+  recommended_temp = 0;
+  $.ajax({
+    type: "POST",
+    url: "/get_temp_val",
+    success: function(ajax_response) {
+      recommended_temp = ajax_response;
+      console.log("recommended_temp: ", ajax_response);
+      moveTriangleByData(ajax_response);
+    } 
+  });
+  console.log("recommended_temp.......", recommended_temp)
+}
+
+///////////////////// end: functions /////////////////////////
+
+
